384a93acecd68042d50da9e35d7380ef7d0b45b4
Byeongmin Choi <28807922+b8choi@users.noreply.github.com>
2021-07-15 22:44:07 +0900

    [modify file] fix es_setting.py

--- es_setting.py
+++ es_setting.py
@@ -15,86 +15,86 @@ print(es.info()) # 정상적으로 출력이 되면, 엘라스틱 서치 연결 
 
 # %%
 # 엘라스틱 서치에 문서를 저장할 index를 생성.
-# es.indices.create(index = 'document',
-#                   body = {
-#                       'settings':{
-#                           'analysis':{
-#                               'analyzer':{
-#                                   'my_analyzer':{
-#                                       "type": "custom",
-#                                       'tokenizer':'nori_tokenizer',
-#                                       'decompound_mode':'mixed',
-#                                       'stopwords':'_korean_',
-#                                       'synonyms':'_korean_',
-#                                       "filter": ["lowercase",
-#                                                  "my_shingle_f",
-#                                                  "nori_readingform",
-#                                                  "cjk_bigram",
-#                                                  "decimal_digit",
-#                                                  "stemmer",
-#                                                  "trim"]
-#                                   },
-#                                   'kor2eng_analyzer':{
-#                                       'type':'custom',
-#                                       'tokenizer':'nori_tokenizer',
-#                                       'filter': [
-#                                           'trim',
-#                                           'lowercase',
-#                                           'javacafe_kor2eng'
-#                                       ]
-#                                   },
-#                                   'eng2kor_analyzer': {
-#                                       'type': 'custom',
-#                                       'tokenizer': 'nori_tokenizer',
-#                                       'filter': [
-#                                           'trim',
-#                                           'lowercase',
-#                                           'javacafe_eng2kor'
-#                                       ]
-#                                   },
-#                               },
-#                               'filter':{
-#                                   'my_shingle_f':{
-#                                       "type": "shingle"
-#                                   }
-#                               }
-#                           },
-#                           'similarity':{
-#                               'my_similarity':{
-#                                   'type':'BM25',
-#                               }
-#                           }
-#                       },
-#                       'mappings':{
-#                           'properties':{
-#                               'title':{
-#                                   'type':'keyword',
-#                                   'copy_to':['title_kor2eng','title_eng2kor']
-#                               },
-#                               'title_kor2eng': {
-#                                   'type': 'text',
-#                                   'analyzer':'my_analyzer',
-#                                   'search_analyzer': 'kor2eng_analyzer'
-#                               },
-#                               'title_eng2kor': {
-#                                   'type': 'text',
-#                                   'analyzer':'my_analyzer',
-#                                   'search_analyzer': 'eng2kor_analyzer'
-#                               },
-#                               'text':{
-#                                   'type':'text',
-#                                   'analyzer':'my_analyzer',
-#                                   'similarity':'my_similarity',
-#                               },
-#                               'text_origin': {
-#                                   'type': 'text',
-#                                   'analyzer': 'my_analyzer',
-#                                   'similarity': 'my_similarity'
-#                               }
-#                           }
-#                       }
-#                   }
-#                   )
+es.indices.create(index = 'document',
+                  body = {
+                      'settings':{
+                          'analysis':{
+                              'analyzer':{
+                                  'my_analyzer':{
+                                      "type": "custom",
+                                      'tokenizer':'nori_tokenizer',
+                                      'decompound_mode':'mixed',
+                                      'stopwords':'_korean_',
+                                      'synonyms':'_korean_',
+                                      "filter": ["lowercase",
+                                                 "my_shingle_f",
+                                                 "nori_readingform",
+                                                 "cjk_bigram",
+                                                 "decimal_digit",
+                                                 "stemmer",
+                                                 "trim"]
+                                  },
+                                  'kor2eng_analyzer':{
+                                      'type':'custom',
+                                      'tokenizer':'nori_tokenizer',
+                                      'filter': [
+                                          'trim',
+                                          'lowercase',
+                                          'javacafe_kor2eng'
+                                      ]
+                                  },
+                                  'eng2kor_analyzer': {
+                                      'type': 'custom',
+                                      'tokenizer': 'nori_tokenizer',
+                                      'filter': [
+                                          'trim',
+                                          'lowercase',
+                                          'javacafe_eng2kor'
+                                      ]
+                                  },
+                              },
+                              'filter':{
+                                  'my_shingle_f':{
+                                      "type": "shingle"
+                                  }
+                              }
+                          },
+                          'similarity':{
+                              'my_similarity':{
+                                  'type':'BM25',
+                              }
+                          }
+                      },
+                      'mappings':{
+                          'properties':{
+                              'title':{
+                                  'type':'keyword',
+                                  'copy_to':['title_kor2eng','title_eng2kor']
+                              },
+                              'title_kor2eng': {
+                                  'type': 'text',
+                                  'analyzer':'my_analyzer',
+                                  'search_analyzer': 'kor2eng_analyzer'
+                              },
+                              'title_eng2kor': {
+                                  'type': 'text',
+                                  'analyzer':'my_analyzer',
+                                  'search_analyzer': 'eng2kor_analyzer'
+                              },
+                              'text':{
+                                  'type':'text',
+                                  'analyzer':'my_analyzer',
+                                  'similarity':'my_similarity',
+                              },
+                              'text_origin': {
+                                  'type': 'text',
+                                  'analyzer': 'my_analyzer',
+                                  'similarity': 'my_similarity'
+                              }
+                          }
+                      }
+                  }
+                  )
 
 # %%
 # 정상적으로 출력이 되면, index 생성 완료
@@ -104,7 +104,7 @@ print(es.indices.get('document'))
 
 import json
 
-with open('data/namuwiki_baseball.json', 'r') as f:
+with open('data/namuwiki_baseball.json', 'r', encoding='utf-8') as f:
     wiki_data = json.load(f)
 
 # %%
